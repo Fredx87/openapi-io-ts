66323c5810a8edceeefc40025f393fea45c5a9f0
Fredx87 <freddy87@gmail.com>
2021-04-17 16:39:13 +0200

    Removed eslint unused import plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "fp-ts", "unused-imports", "prettier"],
+  plugins: ["@typescript-eslint", "fp-ts", "prettier"],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ["./tsconfig.json", "./examples/tsconfig.json"],
@@ -15,16 +15,5 @@ module.exports = {
   ],
   rules: {
     "fp-ts/no-module-imports": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
   },
 };
