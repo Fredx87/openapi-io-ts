4ce976da08e11295a16ef59eb614d04719948c95
Fredx87 <freddy87@gmail.com>
2021-04-25 01:37:09 +0200

    Refactored cli build

---
+++ packages/cli/src/bin.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import * as yargs from "yargs";
+import { main } from ".";
+
+const argv = yargs
+  .usage("Usage: $0 [options]")
+  .example(
+    "$0 -i ./petstore.yaml",
+    "Generates io-ts files from the schema given in input"
+  )
+  .options({
+    input: {
+      alias: "i",
+      demandOption: true,
+      description: "OpenAPI file to parse",
+    },
+    output: {
+      alias: "o",
+      demandOption: true,
+      description: "Output directory",
+      default: "./out",
+    },
+  }).argv;
+
+main(argv.input as string, argv.output);
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,26 +1,28 @@
-#!/usr/bin/env node
+import { error, log } from "fp-ts/Console";
+import { pipe } from "fp-ts/function";
+import * as RTE from "fp-ts/ReaderTaskEither";
+import * as T from "fp-ts/Task";
+import * as TE from "fp-ts/TaskEither";
+import { codegen } from "./codegen";
+import { Environment } from "./environment";
+import { parse } from "./parser";
 
-import * as yargs from "yargs";
-import { main } from "./main";
+export function main(inputFile: string, outputDir: string): void {
+  const env: Environment = {
+    inputFile,
+    outputDir,
+  };
 
-const argv = yargs
-  .usage("Usage: $0 [options]")
-  .example(
-    "$0 -i ./petstore.yaml",
-    "Generates io-ts files from the schema given in input"
-  )
-  .options({
-    input: {
-      alias: "i",
-      demandOption: true,
-      description: "OpenAPI file to parse",
-    },
-    output: {
-      alias: "o",
-      demandOption: true,
-      description: "Output directory",
-      default: "./out",
-    },
-  }).argv;
+  const program = pipe(parse(), RTE.chain(codegen));
 
-main(argv.input as string, argv.output);
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  pipe(program(env), TE.fold(onLeft, onRight))();
+}
+
+function onLeft(e: Error): T.Task<void> {
+  return T.fromIO(error(e));
+}
+
+function onRight(): T.Task<void> {
+  return T.fromIO(log("Files generated successfully!"));
+}
