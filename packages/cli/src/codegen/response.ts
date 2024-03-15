41b1e3b941f0fdc031acf68d979abd63cc41611b
@anulman <anulman@gmail.com>
2024-03-14 23:13:52 -0400

    allow recursive response components

--- packages/cli/src/codegen/response.ts
+++ packages/cli/src/codegen/response.ts
@@ -1,5 +1,7 @@
 import { pipe } from "fp-ts/function";
-import { ResponseItemOrRef } from "../parser/response";
+import { ParsedItem } from "../parser/common";
+import { ParsedResponse, ResponseItemOrRef } from "../parser/response";
+import { capitalize } from "../utils";
 import { getItemOrRefPrefix, getParsedItem } from "./common";
 import { CodegenRTE } from "./context";
 import * as RTE from "fp-ts/ReaderTaskEither";
@@ -45,6 +47,41 @@ export function generateOperationResponse(
           : gen.printRuntime(type);
 
       return `{ _tag: "JsonResponse", decoder: ${runtimeType}}`;
+    })
+  );
+}
+
+export function generateComponentResponse(
+  itemOrRef: ResponseItemOrRef
+): CodegenRTE<string> {
+  return pipe(
+    getParsedItem(itemOrRef),
+    RTE.map((response: ParsedItem<ParsedResponse>) => {
+      const baseName = capitalize(response.name, "camel");
+      const schemaName = capitalize(response.name, "pascal");
+
+      if (response.item._tag === "ParsedEmptyResponse") {
+        return `export const ${schemaName} = t.never();
+
+          export const ${baseName} = { _tag: "EmptyResponse" };`;
+      }
+
+      if (response.item._tag === "ParsedFileResponse") {
+        return `export const ${schemaName} = t.never();
+
+          export const ${baseName} = { _tag: "FileResponse" };`;
+      }
+
+      const type = response.item.type;
+      const decoder = type.kind === "TypeDeclaration" ? type.type : type;
+
+      return `export const ${schemaName}: t.Type<${schemaName}> = ${gen.printRuntime(
+        decoder
+      )};
+
+        export const ${baseName} = { _tag: "JsonResponse", decoder: ${schemaName} };
+
+        export interface ${schemaName} ${gen.printStatic(decoder)};`;
     })
   );
 }
